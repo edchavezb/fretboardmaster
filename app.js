4728bc66a61f0c858c415720f26af9256677831c
Magpie <edchavez821@gmail.com>
2021-03-02 15:00:17 -0600

    Added stop functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const naturals = ["A", "B", "C", "D", "E", "F", "G"]
-const sharps = ["A\u266F", "C#\u266F", "D#\u266F", "F#\u266F", "G#\u266F"]
+const sharps = ["A\u266F", "C\u266F", "D\u266F", "F\u266F", "G\u266F"]
 const flats = ["A\u266D", "B\u266D", "D\u266D", "E\u266D", "G\u266D"]
 
 const stringDiv = document.getElementById("string-number")
@@ -8,6 +8,9 @@ const startBtn = document.getElementById("start")
 const sharpCheck = document.getElementById("check-sharps")
 const flatCheck = document.getElementById("check-flats")
 const bpm = document.getElementById("set-bpm").value
+
+let metronome = null
+let countOn = false
 
 function myRandom(max, min) {
     return Math.floor(Math.random() * (max-min) + min)
@@ -22,10 +25,18 @@ function stringNote(noteArray) {
 }
 
 startBtn.addEventListener('click', () => {
-    console.log("You clicked")
-    let noteSet = sharpCheck.checked ? naturals.concat(sharps) : naturals;
-    noteSet = flatCheck.checked ? noteSet.concat(flats) : noteSet;
-    setInterval(() => stringNote(noteSet), 60000 / bpm * 4)
+    if (!countOn) {
+        countOn = true
+        startBtn.innerHTML = "Stop"
+        let noteSet = sharpCheck.checked ? naturals.concat(sharps) : naturals;
+        noteSet = flatCheck.checked ? noteSet.concat(flats) : noteSet;
+        metronome = setInterval(() => stringNote(noteSet), 60000 / bpm * 4)
+    } else {
+        console.log("Please stop")
+        countOn = false
+        startBtn.innerHTML = "Start"
+        clearInterval(metronome);
+    }
 })
 
 stringNote(naturals);
