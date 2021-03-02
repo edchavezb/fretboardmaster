20211260b8bff156947bab4a2db64bc5ce46397f
Magpie <edchavez821@gmail.com>
2021-03-02 14:26:03 -0600

    Generates a new note every measure

---
+++ app.js
@@ -0,0 +1,31 @@
+const naturals = ["A", "B", "C", "D", "E", "F", "G"]
+const sharps = ["A\u266F", "C#\u266F", "D#\u266F", "F#\u266F", "G#\u266F"]
+const flats = ["A\u266D", "B\u266D", "D\u266D", "E\u266D", "G\u266D"]
+
+const stringDiv = document.getElementById("string-number")
+const noteDiv = document.getElementById("note-name")
+const startBtn = document.getElementById("start")
+const sharpCheck = document.getElementById("check-sharps")
+const flatCheck = document.getElementById("check-flats")
+const bpm = document.getElementById("set-bpm").value
+
+function myRandom(max, min) {
+    return Math.floor(Math.random() * (max-min) + min)
+}
+
+function stringNote(noteArray) {
+    let string = myRandom(6, 1)
+    let note = noteArray[myRandom(noteArray.length, 0)]
+    console.log(string + ", " + note)
+    stringDiv.innerText = string
+    noteDiv.innerText = note
+}
+
+startBtn.addEventListener('click', () => {
+    console.log("You clicked")
+    let noteSet = sharpCheck.checked ? naturals.concat(sharps) : naturals;
+    noteSet = flatCheck.checked ? noteSet.concat(flats) : noteSet;
+    setInterval(() => stringNote(noteSet), 60000 / bpm * 4)
+})
+
+stringNote(naturals);
