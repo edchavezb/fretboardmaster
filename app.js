3c1c69fe0bd0fc0563e4ac686d9655d3db388ef6
Magpie <edchavez821@gmail.com>
2021-03-11 13:34:19 -0600

    Added support for Wake Lock API

--- app.js
+++ app.js
@@ -29,87 +29,104 @@ let noteSound = null
 let metronome = null
 let beatCount = 1
 let countOn = false
+let wakeLock = null;
 
 function myRandom(max, min) {
-    return Math.floor(Math.random() * (max-min) + min)
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
 function stringNote(noteArray) {
-    string = selectedStrings[myRandom(selectedStrings.length, 0)]
-    note = noteArray[myRandom(noteArray.length, 0)]
-    stringDiv.innerText = string
-    noteDiv.innerText = note
+  string = selectedStrings[myRandom(selectedStrings.length, 0)]
+  note = noteArray[myRandom(noteArray.length, 0)]
+  stringDiv.innerText = string
+  noteDiv.innerText = note
 }
 
 function soundSelector(string, note) {
-    const allTones = note.includes('\u266D') ? natsAndflats : naturals.concat(sharps).sort()
-    const actualString = 7 - string
-    let openString = guitarStrings[actualString - 1]
-    let notesBefore = actualString == 6 || actualString == 5 ? (actualString - 1) * 5 - 1 : (actualString - 1) * 5
-    let startingPoint = allTones.indexOf(openString) + fretOffset
-    let fretDistance = allTones.indexOf(note) >= startingPoint ? 
-            allTones.indexOf(note) - startingPoint 
-            : 12 - (startingPoint - allTones.indexOf(note))
-    return  notesBefore + fretOffset + fretDistance + 1
+  const allTones = note.includes('\u266D') ? natsAndflats : naturals.concat(sharps).sort()
+  const actualString = 7 - string
+  let openString = guitarStrings[actualString - 1]
+  let notesBefore = actualString == 6 || actualString == 5 ? (actualString - 1) * 5 - 1 : (actualString - 1) * 5
+  let startingPoint = allTones.indexOf(openString) + fretOffset
+  let fretDistance = allTones.indexOf(note) >= startingPoint ?
+    allTones.indexOf(note) - startingPoint
+    : 12 - (startingPoint - allTones.indexOf(note))
+  return notesBefore + fretOffset + fretDistance + 1
 }
 
-function metronomeBeat(beat){
-    let noteSet = flatCheck.checked ? natsAndflats : naturals;
-    noteSet = sharpCheck.checked ? noteSet.concat(sharps) : noteSet;
-    document.querySelectorAll(".beat").forEach(oneBeat => oneBeat.classList.remove("beat-elapsed"));
-    switch (beat) {
-        case 1:
-            if (!noteSound) firstClick.play()
-            else noteSound.play()
-            stringNote(noteSet)
-            noteSound = new Audio(`sounds/guitar-sounds/${soundSelector(string, note)}.mp3`)
-            first.classList.add("first-elapsed")
-            beatCount++
-            break;
-        case 2: 
-            first.classList.remove("first-elapsed")
-            second.classList.add("beat-elapsed")
-            beatClick.play()
-            beatCount++
-            break;
-        case 3: 
-            third.classList.add("beat-elapsed")
-            beatClick.play()
-            beatCount++
-            break;
-        case 4:
-            fourth.classList.add("beat-elapsed")
-            beatClick.play()
-            beatCount = 1
-            break;
-    }
+function metronomeBeat(beat) {
+  let noteSet = flatCheck.checked ? natsAndflats : naturals;
+  noteSet = sharpCheck.checked ? noteSet.concat(sharps) : noteSet;
+  document.querySelectorAll(".beat").forEach(oneBeat => oneBeat.classList.remove("beat-elapsed"));
+  switch (beat) {
+    case 1:
+      if (!noteSound) firstClick.play()
+      else noteSound.play()
+      stringNote(noteSet)
+      noteSound = new Audio(`sounds/guitar-sounds/${soundSelector(string, note)}.mp3`)
+      first.classList.add("first-elapsed")
+      beatCount++
+      break;
+    case 2:
+      first.classList.remove("first-elapsed")
+      second.classList.add("beat-elapsed")
+      beatClick.play()
+      beatCount++
+      break;
+    case 3:
+      third.classList.add("beat-elapsed")
+      beatClick.play()
+      beatCount++
+      break;
+    case 4:
+      fourth.classList.add("beat-elapsed")
+      beatClick.play()
+      beatCount = 1
+      break;
+  }
 }
+
+// Function that attempts to request a screen wake lock.
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    /*     wakeLock.addEventListener('release', () => {
+          console.log('Screen Wake Lock released:', wakeLock.released);
+        });
+        console.log('Screen Wake Lock released:', wakeLock.released); */
+  } catch (err) {
+    console.error(`${err.name}: ${err.message}`);
+  }
+};
 
 startBtn.addEventListener('click', () => {
-    if (!countOn) {
-        selectedStrings = [...document.querySelectorAll(".string-check:checked")].map(e => e.value)
-        bpm = document.getElementById("set-bpm").value
-        fretOffset = parseInt(document.getElementById("offset").value)
-        countOn = true
-        startBtn.innerHTML = "Stop"
+  if (!countOn) {
+    selectedStrings = [...document.querySelectorAll(".string-check:checked")].map(e => e.value)
+    bpm = document.getElementById("set-bpm").value
+    fretOffset = parseInt(document.getElementById("offset").value)
+    countOn = true
+    startBtn.innerHTML = "Stop"
 
-        metronomeBeat(beatCount)
-        metronome = setInterval(() => metronomeBeat(beatCount), 60000 / bpm)
+    requestWakeLock();
+    metronomeBeat(beatCount)
+    metronome = setInterval(() => metronomeBeat(beatCount), 60000 / bpm)
 
-    } else {
-        countOn = false
-        startBtn.innerHTML = "Start"
-        clearInterval(metronome);
-    }
+  } else {
+    countOn = false
+    startBtn.innerHTML = "Start"
+    wakeLock.release();
+    wakeLock = null;
+    clearInterval(metronome);
+  }
 })
 
 toggleSettings.addEventListener('click', () => {
-    if (window.getComputedStyle(settingsPanel).getPropertyValue('display') === "none") {
-        console.log("1")
-        settingsPanel.style.display = "block";
-        toggleSettings.innerHTML = "Collapse"
-    } else {
-        settingsPanel.style.display = "none";
-        toggleSettings.innerHTML = "Expand"
-    }
+  if (window.getComputedStyle(settingsPanel).getPropertyValue('display') === "none") {
+    console.log("1")
+    settingsPanel.style.display = "block";
+    toggleSettings.innerHTML = "Collapse"
+  } else {
+    settingsPanel.style.display = "none";
+    toggleSettings.innerHTML = "Expand"
+  }
 })
